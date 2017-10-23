bb98ba37d7d6040e7710182524b6e0db7b6f3c99
UrbanoFonseca <francisco.urbfonseca@gmail.com>
2017-10-24 00:43:08 +0100

    Initial upload.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='normalizator',
+      version='0.1',
+      description='Scaling tools for Python',
+      url='https://github.com/UrbanoFonseca/Normalizator',
+      author='UrbanoFonseca',
+      license='MIT',
+      packages=['normalizator'],
+      zip_safe=False)
