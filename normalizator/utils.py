ddd4c7e8fd421e06f94f238676977ac5c6180b71
UrbanoFonseca <francisco.urbfonseca@gmail.com>
2017-11-23 10:37:55 +0000

    Added LimitSetter to the utils.py file.

--- normalizator/utils.py
+++ normalizator/utils.py
@@ -47,3 +47,26 @@ class ConstantEraser(BaseEstimator, TransformerMixin):
 
     def fit_transform(self, X, y='deprecated'):
         return X[:, np.var(X, axis=0)>1E-20]
+
+
+class LimitSetter(BaseEstimator, TransformerMixin):
+    # Defines the limit range for each variable in the dataset.
+    # Used to guarantee the test dataset is between the range of the training set.
+    def __init__(self, minimum=[], maximum=[]):
+        self.minimum = minimum
+        self.maximum = maximum
+        
+    def fit(self, X, y='deprecated'):
+        self.minimum = np.min(X, axis=0)
+        self.maximum = np.max(X, axis=0)
+        return self
+    
+    def transform(self, X, y='deprecated'):
+        X_ = copy.copy(X)
+        self.X_ = X_[np.all((X_ >= self.minimum) & (X_ <= self.maximum), axis=1)]
+        return self.X_
+    
+    def fit_transform(self, X, y='deprecated'):
+        self.fit(X)
+        self.transform(X)
+        return self.X_
