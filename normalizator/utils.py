dfb64f1acbfd76ac8dfffe07119c8ef1fbcad995
UrbanoFonseca <francisco.urbfonseca@gmail.com>
2017-11-23 15:52:12 +0000

    Update on LimitSetter.

--- normalizator/utils.py
+++ normalizator/utils.py
@@ -50,24 +50,36 @@ class ConstantEraser(BaseEstimator, TransformerMixin):
         return X[:, np.var(X, axis=0)>1E-20]
 
 
+
 class LimitSetter(BaseEstimator, TransformerMixin):
+    # WARNING: Cannot be used within a Pipeline. The current sklearn pipeline
+    # does not support subsampling both X and Y datasets.
+    
     # Defines the limit range for each variable in the dataset.
     # Used to guarantee the test dataset is between the range of the training set.
     def __init__(self, minimum=[], maximum=[]):
         self.minimum = minimum
         self.maximum = maximum
-        
+
     def fit(self, X, y='deprecated'):
         self.minimum = np.min(X, axis=0)
         self.maximum = np.max(X, axis=0)
         return self
-    
+
     def transform(self, X, y='deprecated'):
         X_ = copy.copy(X)
-        self.X_ = X_[np.all((X_ >= self.minimum) & (X_ <= self.maximum), axis=1)]
-        return self.X_
-    
+        id_rows = np.all((X_ >= self.minimum) & (X_ <= self.maximum), axis=1)
+        self.X_ = X_[id_rows]
+        if type(y)!=str:
+            y = copy.copy(y)[id_rows]
+            return self.X_, y
+        else:
+            return self.X_
+
     def fit_transform(self, X, y='deprecated'):
-        self.fit(X)
-        self.transform(X)
-        return self.X_
+        self.fit(X, y)
+        self.transform(X, y)
+        if type(y) == str:
+            return self.X_
+        else:
+            return self.X_, y
